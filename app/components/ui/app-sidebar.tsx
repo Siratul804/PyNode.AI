c8e74e40e77c77b724c93fa015a3b72c817a4597
Siratul Islam <islamsiratul@gmail.com>
2025-02-08 11:57:49 +0600

    ui: added Inroduction page

--- app/components/ui/app-sidebar.tsx
+++ app/components/ui/app-sidebar.tsx
@@ -1,42 +1,37 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import { CircleDollarSign, BookOpen, Key, Smile } from "lucide-react";
 
 import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
   SidebarGroupContent,
-  SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import Link from "next/link";
 
 // Menu items.
 const items = [
   {
-    title: "Home",
-    url: "#",
-    icon: Home,
+    title: "Introduction",
+    url: "/docs",
+    icon: BookOpen,
   },
   {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
+    title: "Free Models",
+    url: "/",
+    icon: Key,
   },
   {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
+    title: "Paid Models",
+    url: "/",
+    icon: CircleDollarSign,
   },
   {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
+    title: "Hugging Face",
+    url: "/",
+    icon: Smile,
   },
 ];
 
@@ -50,10 +45,10 @@ export function AppSidebar() {
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
-                    <a href={item.url}>
+                    <Link href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
-                    </a>
+                    </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
